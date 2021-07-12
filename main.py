e42179b1b20adf78fc85aac78fadc4f2d07d30b0
Taylor Friesen <taylorfriesen@protonmail.com>
2021-07-11 17:07:22 -0700

    added firestore

--- main.py
+++ main.py
@@ -3,27 +3,45 @@ from typing import Optional, Dict
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': project_id,
+})
+
+db = firestore.client()
+
+
 app = FastAPI()
 
 class Tag(BaseModel):
     name: Optional[str] = None
     value: int = 0
 
-# currently storing data in memory!!
-# this will result in data being lost whenever the service shuts down
-# change this to firestore asap
-tags = {"foo": 4}
-
 @app.get("/", response_model = Dict[str, int])
 async def read_all():
-    return tags
+    docs = db.collection(u'tags').stream()
+    result = {}
+    for doc in docs:
+        result[doc.id] = doc.to_dict()[u'sum']
+    return result
 
 @app.post("/")
 async def increment_tag(tag: Tag):
     name = tag.name
     value = tag.value
-    if name in tags:
-        tags[name] += value
+    doc_ref = db.collection(u'tags').document(name)
+    doc = doc_ref.get()
+    if doc.exists:
+        prior_sum = doc.to_dict()[u'sum']
+        doc_ref.set({
+            u'sum': prior_sum + value
+        })
     else:
-        tags[name] = value
-    return tags
+        doc_ref.set({
+            u'sum': value
+        })
