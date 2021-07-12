40b631c722c2454fe41e705cb5657632eacfaec7
Taylor Friesen <taylorfriesen@protonmail.com>
2021-07-11 17:55:29 -0700

    set project id

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+project_id = "tag-counter-319600"
+
 # Use the application default credentials
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
